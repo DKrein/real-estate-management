eb45c041b77630e9940be9ab172fc086e5ab5f80
Douglas Krein <douglaskrein@gmail.com>
2025-09-20 17:39:36 -0300

    feat: add controller to get information regarding a building

---
+++ app/Http/Controllers/BuildingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Building;
+use App\Service\BuildingService;
+use Illuminate\Http\JsonResponse;
+
+class BuildingController extends Controller
+{
+    protected BuildingService $buildingService;
+
+    public function __construct(BuildingService $buildingService)
+    {
+        $this->buildingService = $buildingService;
+    }
+
+    /**
+     * List all buildings.
+     * 
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $buildings = $this->buildingService->getAllBuildings();
+        return response()->json($buildings);
+    }
+
+    /**
+     * List all units inside a building.
+     *
+     * @param Building $building
+     * @return JsonResponse
+     */
+    public function units(Building $building): JsonResponse
+    {
+        $units = $this->buildingService->getUnitsByBuilding($building);
+        return response()->json($units);
+    }
+}
