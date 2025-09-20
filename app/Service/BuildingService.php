9ab154b51a1f48131605604d2f808bf9066d083e
Douglas Krein <douglaskrein@gmail.com>
2025-09-20 17:38:19 -0300

    feat: add the logic to the service

--- app/Service/BuildingService.php
+++ app/Service/BuildingService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Models\Building;
 use App\Repositories\Interface\BuildingRepositoryInterface;
 
 class BuildingService
@@ -9,5 +10,22 @@ class BuildingService
     public function __construct(
       private readonly BuildingRepositoryInterface $buildingRepository,
     ) {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllBuildings()
+    {
+        return $this->buildingRepository->getAll();
+    }
+
+    /**
+     * @param Building $building
+     * @return Building
+     */
+    public function getUnitsByBuilding(Building $building): Building
+    {
+        return $this->buildingRepository->getUnitsByBuilding($building);
     }
 }
