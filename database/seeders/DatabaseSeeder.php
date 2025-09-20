5bcf6e977d1a6f942297f185ee59e70b24b9d127
Douglas Krein <douglaskrein@gmail.com>
2025-09-20 17:12:35 -0300

    feat: create seeder to add some owners, team members, buildings and units

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Building;
+use App\Models\Unit;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 3 owners
+        User::factory(3)->asOwner()->create()->each(function ($owner) {
+            // 3-4 team members per owner
+            User::factory(rand(3,5))->create([
+                'role' => 'member',
+            ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            // 3-4 buildings per owner
+            Building::factory(rand(4,6))->create([
+                'owner_id' => $owner->id,
+            ])->each(function ($building) {
+                // 5-10 units per building
+                Unit::factory(rand(5,10))->create([
+                    'building_id' => $building->id,
+                ]);
+            });
+        });
     }
 }
