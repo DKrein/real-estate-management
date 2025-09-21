bd827ec606782d1e21b80bd268e30201120095a9
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 10:21:02 -0300

    feat: create service to return information about building, units and tasks

--- app/Service/BuildingService.php
+++ app/Service/BuildingService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Models\Building;
 use App\Repositories\Interface\BuildingRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class BuildingService
 {
@@ -13,19 +14,28 @@ class BuildingService
     }
 
     /**
-     * @return mixed
+     * @return Collection
      */
-    public function getAllBuildings()
+    public function getAllBuildings(): Collection
     {
         return $this->buildingRepository->getAll();
     }
 
     /**
      * @param Building $building
-     * @return Building
+     * @return Collection
      */
-    public function getUnitsByBuilding(Building $building): Building
+    public function getUnitsByBuilding(Building $building): Collection
     {
         return $this->buildingRepository->getUnitsByBuilding($building);
+    }
+
+    /**
+     * @param Building $building
+     * @return Collection
+     */
+    public function getTasksByBuilding(Building $building): Collection
+    {
+        return $this->buildingRepository->getTasksByBuilding($building);
     }
 }
