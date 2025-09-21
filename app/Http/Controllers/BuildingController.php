6cc60c8c5e7e534a78a4c9388c51b559475c5429
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 15:18:34 -0300

    refactor: change function to use the one with filter

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Building;
 use App\Service\BuildingService;
+use Exception;
 use Illuminate\Http\JsonResponse;
 
 class BuildingController extends Controller
@@ -22,8 +23,15 @@ class BuildingController extends Controller
      */
     public function index(): JsonResponse
     {
-        $buildings = $this->buildingService->getAllBuildings();
-        return response()->json($buildings);
+        try {
+            $buildings = $this->buildingService->getAllBuildings();
+            return response()->json($buildings);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Something went wrong',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -34,10 +42,23 @@ class BuildingController extends Controller
      */
     public function show(Building $building): JsonResponse
     {
-        $buildings = $this->buildingService->getBuildingTasksAndComments($building);
-        return response()->json($buildings);
-    }
+        try {
+            $taskFilters = request()->only([
+                'status',
+                'assigned_user_id',
+                'created_from',
+                'created_to',
+            ]);
 
+            $buildingsData = $this->buildingService->getBuildingTasksAndCommentsFiltered($building, $taskFilters);
+            return response()->json($buildingsData);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Something went wrong',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
 
     /**
      * List all units inside a building.
@@ -47,8 +68,15 @@ class BuildingController extends Controller
      */
     public function units(Building $building): JsonResponse
     {
-        $units = $this->buildingService->getUnitsByBuilding($building);
-        return response()->json($units);
+        try {
+            $units = $this->buildingService->getUnitsByBuilding($building);
+            return response()->json($units);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Something went wrong',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -59,7 +87,14 @@ class BuildingController extends Controller
      */
     public function tasks(Building $building): JsonResponse
     {
-        $units = $this->buildingService->getTasksByBuilding($building);
-        return response()->json($units);
+        try {
+            $units = $this->buildingService->getTasksByBuilding($building);
+            return response()->json($units);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Something went wrong',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
 }
