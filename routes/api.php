125a5550ee945b9c1a6118e9542c3eba4840b74f
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 10:22:08 -0300

    feat: exposing routes to the API

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('buildings', [BuildingController::class, 'index']);
 Route::get('buildings/{building}/units', [BuildingController::class, 'units']);
+Route::get('buildings/{building}/tasks', [BuildingController::class, 'tasks']);
