715ab74390818841f416a3fd7d6668910e3ced39
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 12:22:59 -0300

    feat: add routes to add a new task a comment to a task

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\BuildingController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('buildings', [BuildingController::class, 'index']);
 Route::get('buildings/{building}/units', [BuildingController::class, 'units']);
 Route::get('buildings/{building}/tasks', [BuildingController::class, 'tasks']);
+
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::post('/tasks/{task}/comments', [CommentController::class, 'store']);
