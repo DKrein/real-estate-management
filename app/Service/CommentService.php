2bb396797908e1c71b412ff6e59333f1a9c2523f
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 10:52:03 -0300

    feat: add service logic to use the create on the repository

--- app/Service/CommentService.php
+++ app/Service/CommentService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Models\Comment;
 use App\Repositories\Interface\CommentRepositoryInterface;
 
 class CommentService
@@ -9,5 +10,14 @@ class CommentService
     public function __construct(
         private readonly CommentRepositoryInterface $commentRepository,
     ) {
+    }
+
+    /**
+     * @param array $data
+     * @return Comment
+     */
+    public function create(array $data): Comment
+    {
+        return $this->commentRepository->create($data);
     }
 }
--- app/Service/TaskService.php
+++ app/Service/TaskService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Models\Task;
 use App\Repositories\Interface\TaskRepositoryInterface;
 
 class TaskService
@@ -9,5 +10,14 @@ class TaskService
     public function __construct(
         private readonly TaskRepositoryInterface $taskRepository,
     ) {
+    }
+
+    /**
+     * @param array $data
+     * @return Task
+     */
+    public function create(array $data): Task
+    {
+        return $this->taskRepository->create($data);
     }
 }
