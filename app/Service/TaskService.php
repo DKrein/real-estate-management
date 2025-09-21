c2310a43b1b5a9be992d7c01ccd8bec0ace00610
Douglas Krein <douglaskrein@gmail.com>
2025-09-21 11:52:59 -0300

    feat: add a function to return all tasks

--- app/Service/TaskService.php
+++ app/Service/TaskService.php
@@ -4,12 +4,18 @@ namespace App\Service;
 
 use App\Models\Task;
 use App\Repositories\Interface\TaskRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class TaskService
 {
     public function __construct(
         private readonly TaskRepositoryInterface $taskRepository,
     ) {
+    }
+
+    public function getAllTasks(): Collection
+    {
+        return $this->taskRepository->getAll();
     }
 
     /**
